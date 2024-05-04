bf04fafba9a6607bf591f628a289613edc4feb38
podafool <167709327+podafool@users.noreply.github.com>
2024-05-04 10:30:51 +0530

    Update play.py

--- ANNIEMUSIC/plugins/play/play.py
+++ ANNIEMUSIC/plugins/play/play.py
@@ -36,7 +36,6 @@ from ANNIEMUSIC.utils.stream.stream import stream
    filters.command(["play", "vplay", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce"] ,prefixes=["/", "!"])
             
     & filters.group
-    & ~BANNED_USERS
 )
 @PlayWrapper
 async def play_commnd(
