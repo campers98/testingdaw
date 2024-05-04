cab2a41d3564c529d17cd5224c43ef3bbcbcc80d
podafool <167709327+podafool@users.noreply.github.com>
2024-05-04 13:12:03 +0530

    Update goodnight.py

--- ANNIEMUSIC/plugins/Yumi/goodnight.py
+++ ANNIEMUSIC/plugins/Yumi/goodnight.py
@@ -23,8 +23,8 @@ def goodnight_command_handler(client: Client, message: Message):
 # Function to get a random sticker
 def get_random_sticker():
     stickers = [
-        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E",
-        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E",
+        "CAACAgUAAxkBAAEMDFJmNeZlg7gWGm5F6DmFYnL--b_pZwACqQQAAusWiVQORprTsz5kHTQE",
+        "CAACAgQAAx0CdbvzLQACOrtmNeXYAAGQu3Af_N78dZ3NoecollYAAl0MAAJj-6lShY3fdbd5wpQ0BA",
         "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA",
         "CAACAgUAAx0CcmOuMwACldVlwn9ZHHF2-S-CuMSYabwwtVGC3AACOAkAAoqR2VYDjyK6OOr_Px4E",
         "CAACAgIAAx0Ce9_hCAACaFVlwn-fG58GKoEmmZpVovxEj4PodAACfwwAAqozQUrt2xSTf5Ac4h4E",
