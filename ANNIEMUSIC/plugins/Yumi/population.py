d4d4e356e5ce26de5479eb52f117d242d738aa24
podafool <167709327+podafool@users.noreply.github.com>
2024-05-04 13:49:37 +0530

    Update population.py

--- ANNIEMUSIC/plugins/Yumi/population.py
+++ ANNIEMUSIC/plugins/Yumi/population.py
@@ -24,17 +24,17 @@ def country_command_handler(client: Client, message: Message):
             population = country_info[0].get("population", "N/A")
 
             response_text = (
-                f"Country Information\n\n"
-                f"Name: {country_name}\n"
-                f"Capital: {capital}\n"
-                f"Population: {population}"
+                f"➷➶ Country Information ➷➶\n\n"
+                f"︻ Name ︻: {country_name}\n"
+                f"︻ Capital ︻: {capital}\n"
+                f"︻ Population ︻: {population}"
             )
         else:
             response_text = "Error fetching country information from the API."
     except requests.exceptions.HTTPError as http_err:
         response_text = f"HTTP error occurred Enter correct Country code"
     except Exception as err:
-        response_text = f" Error @iam_daxc"
+        response_text = f" Error @SVD"
 
     # Send the response to the Telegram chat
     message.reply_text(response_text)
