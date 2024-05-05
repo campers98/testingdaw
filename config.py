06f2d1725051b5a394ce21ffe1ba5769f50ed231
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-06 00:23:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Hypernetwork123:Hypernetwor
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001975251757))
+LOGGER_ID = int(getenv("LOGGER_ID", -1001743709729))
 
 # ------------------------------------------------
 GPT_API = getenv("sk-proj-mywH9SfPWQuT2QhGLhKkT3BlbkFJUafYpIlrEpoYbz9rcy8L")
