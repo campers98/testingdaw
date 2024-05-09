eeb62f54ed44ec9f1a461bca7efd39d3f641251a
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-09 08:05:16 +0530

    Update gnmsg.py

--- ANNIEMUSIC/plugins/tools/gnmsg.py
+++ ANNIEMUSIC/plugins/tools/gnmsg.py
@@ -6,7 +6,7 @@ from ANNIEMUSIC import app
 
 
 ######### GOOD NIGHT 
-@app.on_message(filters.command(["gn","n","oodnightu","oodu Nightu","ood night"], prefixes=["/","g","G"]))
+@app.on_message(filters.command(["oodnightu","oodu Nightu","ood night"], prefixes=["/","g","G"]))
 def goodnight_command_handler(_, message):
     sender = message.from_user.mention
     send_sticker = random.choice([True, False])
